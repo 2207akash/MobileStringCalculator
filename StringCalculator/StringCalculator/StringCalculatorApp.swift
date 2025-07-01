a83b13162566ea05c8f4818d8376c0e4aacde03b
2207akash <2207akash@gmail.com>
2025-07-01 21:54:50 +0530

    Basic Project Setup

---
+++ StringCalculator/StringCalculator/StringCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringCalculatorApp.swift
+//  StringCalculator
+//
+//  Created by Akash Sen on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct StringCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
