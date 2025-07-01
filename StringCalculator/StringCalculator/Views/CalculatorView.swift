ffe1e41843d9581879acd01fb4a2c0f29d4bdf71
2207akash <2207akash@gmail.com>
2025-07-01 23:52:32 +0530

    Refactor UI code

--- StringCalculator/StringCalculator/Views/CalculatorView.swift
+++ StringCalculator/StringCalculator/Views/CalculatorView.swift
@@ -12,38 +12,29 @@ struct CalculatorView: View {
     @State private var expression = ""
     @State private var expressionErrorString: String?
     
+    // MARK: Main Content
     var body: some View {
-        VStack {
-            inputView
-            outputView
+        NavigationStack {
+            VStack {
+                VStack(alignment: .center) {
+                    inputView
+                    outputView
+                }
+            }
+            .navigationTitle("String Calculator")
         }
+    }
+    
+    private var headerView: some View {
+        Text("String Calculator")
+            .font(.largeTitle)
+            .foregroundStyle(Color.black)
     }
     
     private var inputView: some View {
         VStack {
-            TextField(
-                "Enter your expression here",
-                text: $expression
-            )
-            .font(.body)
-            .keyboardType(.asciiCapable)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .overlay {
-                if expressionErrorString != nil {
-                    RoundedRectangle(cornerRadius: 4)
-                        .strokeBorder(Color.red)
-                }
-            }
-            .padding()
-            
-            Button("Calculate") {
-                do {
-                    expressionErrorString = nil
-                    try vm.add(expression)
-                } catch (let error) {
-                    expressionErrorString = error.localizedDescription
-                }
-            }
+            inputTextField
+            calculateButton
         }
     }
     
@@ -58,6 +49,38 @@ struct CalculatorView: View {
             }
         }
         .padding(.top)
+    }
+    
+}
+
+private extension CalculatorView {
+    // MARK: Input Views
+    private var inputTextField: some View {
+        TextField(
+            "Enter your expression here",
+            text: $expression
+        )
+        .font(.body)
+        .keyboardType(.asciiCapable)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .overlay {
+            if expressionErrorString != nil {
+                RoundedRectangle(cornerRadius: 4)
+                    .strokeBorder(Color.red)
+            }
+        }
+        .padding()
+    }
+    
+    private var calculateButton: some View {
+        Button("Calculate") {
+            do {
+                expressionErrorString = nil
+                try vm.add(expression)
+            } catch (let error) {
+                expressionErrorString = error.localizedDescription
+            }
+        }
     }
 }
 
